6493a24509b6962d7d8da54a3c2b9efb5cedc74b
Alan Suarez <alansuarez641@hotmail.com>
2023-08-16 21:11:48 -0300

    Upload the totality archives

---
+++ Pages/Peluche/js/main.js
@@ -0,0 +1,7 @@
+function redirigirCompra() {
+    // URL de la página de destino
+    const urlDestino = 'https://w.app/MesaInfinitasRosarioPeluche'; // Reemplaza con la URL de tu página de destino
+
+    // Redirigir a la página de destino en una nueva pestaña o ventana
+    window.open(urlDestino, '_blank');
+}
