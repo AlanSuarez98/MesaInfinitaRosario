6a24fe3a25dad7039650e5c65fd7cbac18f67224
Alan Suarez <alansuarez641@hotmail.com>
2024-01-20 09:30:47 -0300

    Update main.js

--- Pages/Madera/js/main.js
+++ Pages/Madera/js/main.js
@@ -1,8 +1,3 @@
 function redirigirCompra() {
-  // URL de la página de destino
-  const urlDestino =
-    "https://wa.me/543412143089?text=Hola,%20quiero%20comprar%20la%20*Mesa%20Madera*!"; // Reemplaza con la URL de tu página de destino
-
-  // Redirigir a la página de destino en una nueva pestaña o ventana
-  window.open(urlDestino, "_blank");
+  alert("Método de contacto dado de baja");
 }
